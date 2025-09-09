75649bbebcc4d24f845da966160456a2087c70f9
heliosssssssssss <adam.bell.21@portlaoisecbs.com>
2025-09-09 14:16:51 +0100

    rrrr

---
+++ classwork/cw-random.py
@@ -0,0 +1,12 @@
+print("Average height calculator")
+print("=========================")
+
+h1 = float(input("Enter first height (cm) : "))
+h2 = float(input("Enter first height (cm) : "))
+h3 = float(input("Enter first height (cm) : "))
+h4 = float(input("Enter first height (cm) : "))
+h5 = float(input("Enter first height (cm) : "))
+
+avgHeight = (h1+h2+h3+h4+h5) / 5
+
+print(f"The average height is {avgHeight}cm")
